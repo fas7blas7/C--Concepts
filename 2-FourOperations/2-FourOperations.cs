1faed52490bf239cb78d002be0761974438f6ba3
Martin Kirov <fas7blas7@gmail.com>
2025-03-05 21:55:05 +0200

    Adding FourOperations task

---
+++ 2-FourOperations/2-FourOperations.cs
@@ -0,0 +1,12 @@
+﻿double num1 = double.Parse(Console.ReadLine());
+double num2 = double.Parse(Console.ReadLine());
+
+double sumResult = num1 + num2;
+double subResult = num1 - num2;
+double mulResult = num1 * num2;
+double divResult = num1 / num2;
+
+Console.WriteLine($"{num1:f2} + {num2:f2} = {sumResult:f2}");
+Console.WriteLine($"{num1:f2} - {num2:f2} = {subResult:f2}");
+Console.WriteLine($"{num1:f2} * {num2:f2} = {mulResult:f2}");
+Console.WriteLine($"{num1:f2} / {num2:f2} = {divResult:f2}");
