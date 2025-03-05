24e6ab88a5a0285df962fe24b99990831d10cd71
Martin Kirov <fas7blas7@gmail.com>
2025-03-05 21:59:36 +0200

    Adding Tiles task

---
+++ 4-Tiles/4-Tiles.cs
@@ -0,0 +1,10 @@
+﻿double width = double.Parse(Console.ReadLine());
+double height = double.Parse(Console.ReadLine());
+double tileWidth = double.Parse(Console.ReadLine());
+double tileHeigth = double.Parse(Console.ReadLine());
+double bathroomArea = width * height;
+double bathroomAreaTotalSurplus= bathroomArea *= 1.1;
+double tileArea = tileWidth * tileHeigth;
+double tilesNeeded = bathroomAreaTotalSurplus / tileArea;
+
+Console.WriteLine(Math.Round(tilesNeeded));
