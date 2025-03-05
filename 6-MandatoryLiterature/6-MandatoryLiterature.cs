92e44b3a8b327dba731411518ca67714470a6580
Martin Kirov <fas7blas7@gmail.com>
2025-03-05 22:00:39 +0200

    Adding MandatoryLiterature task

---
+++ 6-MandatoryLiterature/6-MandatoryLiterature.cs
@@ -0,0 +1,8 @@
+﻿int pages = int.Parse(Console.ReadLine());
+int pagesPerHour = int.Parse(Console.ReadLine());
+int days = int.Parse(Console.ReadLine());
+
+int hoursPerDay = pages / pagesPerHour;
+int hoursToRead = hoursPerDay / days;
+
+Console.WriteLine(hoursToRead);
