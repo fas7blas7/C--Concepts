87a560f6247aa00b3f3a86e33b629e08791f9bbe
Martin Kirov <fas7blas7@gmail.com>
2025-03-05 22:03:20 +0200

    Adding SuppliesForSchool task

---
+++ 7-SuppliesForSchool/7-SuppliesForSchool.cs
@@ -0,0 +1,12 @@
+﻿int numberOfPackagesOfPens = int.Parse(Console.ReadLine());
+int numberOfPackagesOfMarkers = int.Parse(Console.ReadLine());
+int litersOfBoardCleaner =  int.Parse(Console.ReadLine());
+int discount = int.Parse(Console.ReadLine());
+double priceOfPackageOfPens = 5.80;
+double priceOfPackageOfMarkers = 7.20;
+double priceOfLiterOfBoardCleaner = 1.20;
+
+double totalCost = numberOfPackagesOfPens * priceOfPackageOfPens + numberOfPackagesOfMarkers * priceOfPackageOfMarkers + litersOfBoardCleaner * priceOfLiterOfBoardCleaner;
+double discountedTotalCost = totalCost - totalCost * discount / 100;
+
+Console.WriteLine($"{discountedTotalCost:F3}");
