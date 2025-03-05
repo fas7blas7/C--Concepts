3603e3e2c9245a8bc8e7543c168134dcd07afcac
Martin Kirov <fas7blas7@gmail.com>
2025-03-05 22:00:14 +0200

    Adding DepositCalc task

---
+++ 5-DepositCalculator/5-DepositCalculator.cs
@@ -0,0 +1,6 @@
+﻿double deposit = double.Parse(Console.ReadLine());
+int monthsTerm = int.Parse(Console.ReadLine());
+double interestRate = double.Parse(Console.ReadLine());
+double interestRateConverted = interestRate *= 0.01;
+double interest = deposit + monthsTerm * (deposit * interestRateConverted) / 12;
+Console.WriteLine($"{interest:f2}");
