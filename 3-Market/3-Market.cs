7c839fd2aecaee572bf9f4da808e9a9846716841
Martin Kirov <fas7blas7@gmail.com>
2025-03-05 21:55:31 +0200

    Adding Market task

---
+++ 3-Market/3-Market.cs
@@ -0,0 +1,10 @@
+﻿double tomatoPrice = double.Parse(Console.ReadLine());
+double tomatoWeight = double.Parse(Console.ReadLine());
+double cucumberPrice = double.Parse(Console.ReadLine());
+double cucumberWeight = double.Parse(Console.ReadLine());
+
+double potatoCost = tomatoPrice * tomatoWeight;
+double cucumberCost = cucumberPrice * cucumberWeight;
+double totalCost = potatoCost + cucumberCost;
+
+Console.WriteLine($"{totalCost:f2}");
